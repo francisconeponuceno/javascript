43dd7dfd698c71c4028cb0ee793405dde1be8231
francisconeponuceno <120109535+francisconeponuceno@users.noreply.github.com>
2023-10-11 10:43:03 -0300

    atualizacao

---
+++ aula013/script.js
@@ -0,0 +1,15 @@
+var c = 1
+/*while (c <= 10) {
+    window.document.write(`BOM DIA! PASSO ${c} </br>`)
+    c++
+}
+*/
+/*do  {
+    window.document.write(`COE GALERA! PASSO ${c} </br>`)
+    c++
+} while (c <= 10)
+*/
+
+for (var c =1;c <= 10; c++) {
+    window.document.write(`BOM DIA! TESTE COM FOR. PASSO ${c} </br>`)
+}
