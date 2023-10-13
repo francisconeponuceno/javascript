4c823f3d7a45b66db20b451a403021efd0ae3730
francisconeponuceno <120109535+francisconeponuceno@users.noreply.github.com>
2023-10-13 10:36:04 -0300

    atualizacao

--- aula014/ex016/script.js
+++ aula014/ex016/script.js
@@ -6,14 +6,27 @@ function contar() {
     
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossível contar'
     } else {
-        res.innerHTML = 'Contador '
+        res.innerHTML = 'Contando: <br> '
         var i = Number(ini.value)
         var f = Number(fim.value)
         var p = Number(passo.value)
-
-        for (var c = i; c <= f; c += p) {
-            res.innerHTML += ` ${c} \u{1F449}`
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            // Contagem crescente
+            for (var c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        
+        } else {
+            // Contagem regressiva
+            for (var c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
         }
         res.innerHTML += `\u{1f3c1}`
     }
--- aula014/ex017/script.js
+++ aula014/ex017/script.js
@@ -1,49 +1,18 @@
-function verificar() {
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = document.getElementById(`txtano`)
-    var res = document.querySelector(`div#res`)
-    if (fano.value.length == 0 || fano.value > ano) {
-        window.alert(`[ERRO] Verifique os dados e tente novamente!`)
+function tabuada() {
+    var num = document.getElementById('txtn')
+    var tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('por favor, digite um número!')   
     } else {
-        var fsex = document.getElementsByName(`radsex`)
-        var idade = ano - Number(fano.value)
-        var genero = ''
-        var img = document.createElement('img')
-        img.setAttribute('id', 'foto')
-        if (fsex[0].checked) {
-            genero = 'Homem'
-            if (idade >=0 && idade < 10) {
-                //criança
-                img.setAttribute('src', 'h.crianca.png')
-            } else if (idade < 21) {
-                //Jovem
-                img.setAttribute('src','h.jovem.png')
-            } else if (idade < 50) {
-                //Adulto
-                img.setAttribute('src', 'h.adulto.png')
-            } else {
-                //Idoso
-                img.setAttribute('src','h.idoso.png')
-            }
-        } else if (fsex[1].checked) {
-            genero = 'Mulher'
-            if (idade >=0 && idade < 10) {
-                //criança
-                img.setAttribute('src','m.crianca.png')
-            } else if (idade < 21) {
-                //Jovem
-                img.setAttribute('src','m.jovem.png')
-            } else if (idade < 50) {
-                //Adulto
-                img.setAttribute('src','m.adulta.png')
-            } else {
-                //Idoso
-                img.setAttribute('src','m.idosa.png')
-            }
+        var n = Number(num.value)
+        var c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
         }
-        res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos. </br>`
-        res.appendChild(img)
     }
 }
